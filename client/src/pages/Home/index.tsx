386c23bc39c643640eb2d293a3ed97a38d656279
Ruan Fernandes <ruan_guimaraes2004@hotmail.com>
2022-06-24 14:45:11 -0300

    .

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -12,13 +12,18 @@ export default function Home(props: HomeProps) {
 
     const apiURL = "http://localhost:3001/navbar/";
     const dataNames = ["id", "text", "href", "deletable", "disabled"];
+    const options = {
+        editable: true,
+        detetable: true,
+        addable: true
+    }
 
     return (
         <>
             <Navbar handleThemeChange={props.ThemeChangingHandler} selectedTheme={props.Theme} />
             <HomeContainer>
                 <div className="bg-slate-300 dark:bg-slate-500 h-full rounded-3xl p-10">
-                    <GenericRecords apiURL={apiURL} dataNames={dataNames} />
+                    <GenericRecords apiURL={apiURL} dataNames={dataNames} options={options} />
                 </div>
             </HomeContainer>
         </>
--- client/src/pages/components/GenericRecords.tsx
+++ client/src/pages/components/GenericRecords.tsx
@@ -1,14 +1,23 @@
 import { useEffect, useState } from "react";
 
+type GenericRecordsOptions = {
+    editable?: boolean;
+    detetable?: boolean;
+    addable?: boolean;
+}
+
 type GenericRecordsProps = {
     dataNames: string[];
     apiURL: string;
+    options?: GenericRecordsOptions;
 }
+
 
 export default function GenericRecords(props: GenericRecordsProps) {
 
     const [data, setData] = useState([]);
 
+    const options: GenericRecordsOptions = props.options || { editable: true, detetable: true, addable: true };
     const dataNames = props.dataNames;
     const apiUrl = props.apiURL;
 
@@ -18,10 +27,27 @@ export default function GenericRecords(props: GenericRecordsProps) {
             .then(data => setData(data));
     }, [apiUrl]);
 
+    const handleEditButton = () => {
+
+    };
+
+    const handleDeleteButton = () => {
+
+    };
+
+    const handleAddableButton = () => {
+
+    };
+
     return (<>
+
+        {/* Table Rendering */}
         <table className="table-auto border-separate border-spacing-2 border border-slate-600 dark:border-slate-600 w-full">
             <thead>
                 <tr>
+                    {
+                        (options.editable ? <th className="px-4 py-2">&nbsp;</th> : null)
+                    }
                     {
                         dataNames.map((item, index) => {
                             return <th className="border border-slate-600 px-5 dark:text-white dark:border-slate-500 dark:bg-slate-900" key={index} style={{ textTransform: 'capitalize' }}>{item}</th>;
@@ -35,9 +61,12 @@ export default function GenericRecords(props: GenericRecordsProps) {
                         return (<>
                             <tr key={index}>
                                 {
-                                    dataNames.map((itm, index) => {
+                                    (options.editable ? <td className="border border-slate-600 px-5 bg-slate-400 text-black text-center dark:text-white dark:border-slate-500 dark:bg-slate-700">aa</td> : null)
+                                }
+                                {
+                                    dataNames.map((itm, indx) => {
                                         return (<>
-                                            <td className="border border-slate-600 px-5 bg-slate-400 text-black text-center dark:text-white dark:border-slate-500 dark:bg-slate-700" key={index}>{item[itm]}</td>
+                                            <td className="border border-slate-600 px-5 bg-slate-400 text-black text-center dark:text-white dark:border-slate-500 dark:bg-slate-700" key={indx}>{item[itm]}</td>
                                         </>)
                                     })
                                 }
